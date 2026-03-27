e9badf20469c5b7086beacc66bfaed4621df25e7
Gabriel Lopes da Silva <supergabriel1533@gmail.com>
2026-03-26 23:09:44 -0300

    Setup inicial do Django e implementação dos Models do Banco de Dados

---
+++ core/models.py
@@ -0,0 +1,55 @@
+from django.db import models
+
+class Categoria(models.Model):
+    nome_categoria = models.CharField(max_length=100)
+    descricao = models.TextField(blank=True, null=True)
+
+    def __str__(self):
+        return self.nome_categoria
+
+class Fornecedor(models.Model):
+    nome_fantasia = models.CharField(max_length=200)
+    cnpj = models.CharField(max_length=18, unique=True)
+    telefone = models.CharField(max_length=15)
+    email = models.EmailField()
+
+    def __str__(self):
+        return self.nome_fantasia
+
+class Produto(models.Model):
+    nome_produto = models.CharField(max_length=200)
+    categoria = models.ForeignKey(Categoria, on_delete=models.CASCADE)
+    fornecedor = models.ForeignKey(Fornecedor, on_delete=models.CASCADE)
+    preco_custo = models.DecimalField(max_digits=10, decimal_places=2)
+    preco_venda = models.DecimalField(max_digits=10, decimal_places=2)
+    quantidade_estoque = models.DecimalField(max_digits=10, decimal_places=3)
+    unidade_medida = models.CharField(max_length=10) # Ex: KG, UN, SACA
+
+    def __str__(self):
+        return self.nome_produto
+
+class Cliente(models.Model):
+    nome = models.CharField(max_length=200)
+    cpf = models.CharField(max_length=14, unique=True)
+    telefone = models.CharField(max_length=15)
+    email = models.EmailField()
+
+    def __str__(self):
+        return self.nome
+
+class Venda(models.Model):
+    data_venda = models.DateTimeField(auto_now_add=True)
+    cliente = models.ForeignKey(Cliente, on_delete=models.SET_NULL, null=True)
+    valor_total = models.DecimalField(max_digits=10, decimal_places=2, default=0)
+
+    def __str__(self):
+        return f"Venda {self.id} - {self.data_venda}"
+
+class ItensVenda(models.Model):
+    venda = models.ForeignKey(Venda, on_delete=models.CASCADE, related_name='itens')
+    produto = models.ForeignKey(Produto, on_delete=models.PROTECT)
+    quantidade = models.DecimalField(max_digits=10, decimal_places=3)
+    preco_unitario = models.DecimalField(max_digits=10, decimal_places=2)
+
+    def __str__(self):
+        return f"{self.quantidade} x {self.produto.nome_produto}"
