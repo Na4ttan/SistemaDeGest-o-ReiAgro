c0a5944ea738c03c764983bb57d6b6ea1d6a1690
Na4ttan <natannatan2010@hotmail.com>
2026-05-04 23:50:07 -0300

    senha do email trocada e ocultada

--- reiagro/settings.py
+++ reiagro/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 
 import os
 from pathlib import Path
+from dotenv import load_dotenv
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -135,5 +137,5 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'reiagro6@gmail.com'
-EMAIL_HOST_PASSWORD = 'gmvherldhtwwmhst'
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD') // antiga senha foi trocada
 DEFAULT_FROM_EMAIL = 'Rei Agro <reiagro6@gmail.com>'
